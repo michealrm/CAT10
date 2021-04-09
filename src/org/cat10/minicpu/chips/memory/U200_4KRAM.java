220949b02ba890a5424c27894c84f08a408482a5
Micheal <8922961+michealrm@users.noreply.github.com>
2021-04-09 17:15:48 -0500

    Removed clocked from memory chips

--- src/org/cat10/minicpu/chips/memory/U200_4KRAM.java
+++ src/org/cat10/minicpu/chips/memory/U200_4KRAM.java
@@ -3,8 +3,6 @@ package org.cat10.minicpu.chips.memory;
 import org.cat10.minicpu.chips.Chip;
 
 /**
- * CLOCKED CHIP
- *
  * Inputs:
  * getInput("MemAddr"), bottom 12 bits from 4-1 MUX in instruction circuit
  * getInput("ChipSelect")
--- src/org/cat10/minicpu/chips/memory/U215_4KEPROM.java
+++ src/org/cat10/minicpu/chips/memory/U215_4KEPROM.java
@@ -5,8 +5,6 @@ import org.cat10.minicpu.chips.Chip;
 import java.nio.ByteBuffer;
 
 /**
- * CLOCKED CHIP
- *
  * Inputs:
  * getInput("MemAddr"), bottom 12 bits from 4-1 MUX in instruction circuit
  * getInput("ChipSelect")
