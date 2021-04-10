ff4544d2a492de42f4d1af51e0021cd5df15b91d
Micheal <8922961+michealrm@users.noreply.github.com>
2021-04-09 23:35:26 -0500

    Implemented registers with Master-Slave D Flip Flop

--- src/org/cat10/minicpu/chips/operations/registers/U10_Register0.java
+++ src/org/cat10/minicpu/chips/operations/registers/U10_Register0.java
@@ -2,6 +2,9 @@ package org.cat10.minicpu.chips.operations.registers;
 
 import org.cat10.minicpu.chips.Chip;
 
+import static org.cat10.minicpu.ChipManager.getChip;
+import static org.cat10.minicpu.util.CAT10Util.Not;
+
 /**
  * Inputs:
  * getChip("U999").getOutput("clock")
@@ -13,10 +16,14 @@ import org.cat10.minicpu.chips.Chip;
  */
 public class U10_Register0 extends Chip {
 
-    boolean masterNandOutput1;
-    boolean masterNandOutput2;
-    boolean slaveNandOutput1;
-    boolean slaveNandOutput2;
+    byte masterNandOutput1a;
+    byte masterNandOutput1b;
+    byte masterNandOutput2a;
+    byte masterNandOutput2b;
+    byte slaveNandOutput1a;
+    byte slaveNandOutput1b;
+    byte slaveNandOutput2a;
+    byte slaveNandOutput2b;
 
     public U10_Register0() {
         super("U10");
@@ -24,6 +31,21 @@ public class U10_Register0 extends Chip {
 
     @Override
     public void evaluateOut() {
+        byte D = getInput("D");
+        byte clock = getChip("U999").getOutput("clock");
+        byte notClock = Not(clock);
+        if(getInput("ChipSelect") != 0) {
+            masterNandOutput1a = Not((byte) (D & notClock));
+            masterNandOutput1b = Not((byte) (notClock & Not(D)));
+            masterNandOutput2b = Not((byte) (masterNandOutput2a & masterNandOutput1b));
+            masterNandOutput2a = Not((byte) (masterNandOutput1a & masterNandOutput2b));
 
+            slaveNandOutput1a = Not((byte) (masterNandOutput2a & clock));
+            slaveNandOutput1b = Not((byte) (clock & Not(masterNandOutput2a)));
+            slaveNandOutput2b = Not((byte) (slaveNandOutput2a & slaveNandOutput1b));
+            slaveNandOutput2a = Not((byte) (slaveNandOutput1a & slaveNandOutput2b));
+
+            putOutput("Q", slaveNandOutput2a);
+        }
     }
 }
---
+++ src/org/cat10/minicpu/util/CAT10Util.java
@@ -0,0 +1,9 @@
+package org.cat10.minicpu.util;
+
+public class CAT10Util {
+
+    public static byte Not(byte b) {
+        return b == 0 ? (byte)1 : (byte)0;
+    }
+
+}
